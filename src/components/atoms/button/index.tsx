2dea02ba834e5ea52ee857b84c47fb828fd7ce32
EsterKarolyne <esterknappverly@gmail.com>
2024-09-23 22:16:59 -0300

    feat: Cria base do projeto

---
+++ src/components/atoms/button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles';
+
+export const index = () => {
+  return (
+    <>
+    <S.Button>index</S.Button>
+    </>
+  )
+}
+
+export default index;
